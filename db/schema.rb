b6756b89496feb8d89e19badd790e26389ef9092
Ignacio Carmach <icarmach@gmail.com>
2013-04-17 16:37:48 -0400

    token  agregado a users, gracias chacon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130417202406) do
+ActiveRecord::Schema.define(:version => 20130417203720) do
 
   create_table "archives", :force => true do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20130417202406) do
     t.string   "salt"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "token"
   end
 
 end
