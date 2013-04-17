9ec6a5713ba45391a571d22aaa7ee163061b8334
Ignacio Carmach <icarmach@gmail.com>
2013-04-17 16:05:05 -0400

    app base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TareApp::Application.config.secret_token = 'ac76af4f3f2f8b8122e4a1066d26e74c6d921e4c691c18c425c05887fc7c58832787e7458d7a934f25687f53a32d920ed6dd8d56274c3834c6543a8529a1deb6'
